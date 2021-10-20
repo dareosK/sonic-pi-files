ff62eea42538fe144f7ee113570c1c19cd00f13b
Dareos Khalili <khalilidareos@gmail.com>
2021-10-20 19:59:44 +0200

    adds snippets

---
+++ sentimental_hackers/beats.rb
@@ -0,0 +1,4 @@
+live_loop :quarteP_beat do
+  sample :bass_hit_c, amp: 2, sustain: rrand(10, 20), lpf: 50
+  sleep 1
+end
--- sentimental_hackers/forest.rb
+++ sentimental_hackers/forest.rb
@@ -3,7 +3,7 @@ live_loop :magic_forest do
     with_fx :ping_pong do
       use_synth :kalimba
       play rrand(:E9, :E15), amp: rrand(20, 30), release: 3
-      sleep rrand(0.03, 0.8)
+      sleep rrand(0.025, 0.25)
     end
   end
 end
