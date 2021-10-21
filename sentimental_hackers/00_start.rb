272d8683b48718b9cffe19680a9a613cfcf1fc41
Dareos Khalili <khalilidareos@gmail.com>
2021-10-21 15:59:29 +0200

    snippets

--- sentimental_hackers/00_start.rb
+++ sentimental_hackers/00_start.rb
@@ -1,9 +1,9 @@
 ##| START
 
 live_loop :vinyl_drizzle do
-  sample :vinyl_hiss , amp: 4, attack: 1, sustain: 10, release: 0.1, rate: -1
+  sample :vinyl_hiss , amp: 2, attack: 1, sustain: 10, release: 0.1, rate: -1
   with_fx :gverb, damp: 1 do
-    sample :vinyl_hiss , amp: 4, attack: 1, sustain: 10, release: 1, rate: -0.5
+    sample :vinyl_hiss , amp: 2, attack: 1, sustain: 10, release: 1, rate: -0.5
     sleep 4
   end
 end
